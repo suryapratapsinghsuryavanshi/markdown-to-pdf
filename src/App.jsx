67319803c4492f3feaedf671fe3f158b134a73e3
kuanoni <tehgamemaker@gmail.com>
2022-11-06 22:45:25 -0800

    feat: added resizable panes and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import react from 'react';
-import './global.css'
+import './global.css';
+
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
+import react from 'react';
 
 export default function App() {
-    return(
-        <react.Fragment>
-            <div className="container">
-                <Header/>
-                <Main/>
-            </div>
-        </react.Fragment>
-    );
+	return (
+		<react.Fragment>
+			<div className='app'>
+				<Header />
+				<Main />
+			</div>
+		</react.Fragment>
+	);
 }
