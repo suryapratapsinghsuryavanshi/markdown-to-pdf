942b40292a09b0599856f2764aae4f7a86382389
Suryapratap Singh Suryavanshi <suryprtaps@gmail.com>
2023-07-05 22:00:03 +0530

    Adding PWA assets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
     <App/>,
     document.getElementById('root')
 );
+
+// Service worker.
+serviceWorkerRegistration.register();
